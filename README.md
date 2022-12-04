9c3f8d95f188b6ded415ba1a58c7912246492032
dece <shgck@pistache.land>
2022-12-04 16:32:14 +0100

    server: implement rename (tougher!)
    Heavily borrows code from jedi-language-server.

--- README.md
+++ README.md
@@ -6,17 +6,18 @@ Minimal Python language server, based on [Jedi][jedi] and [pygls][pygls].
 [jedi]: https://jedi.readthedocs.io/en/latest/index.html
 [pygls]: https://pygls.readthedocs.io/en/latest/index.html
 
-WIP! ⚠ 👷🚧
+Still in development but works on my machine. ✨
 
 Supported features:
 
-| LSP method                    | Description           |
-|-------------------------------|-----------------------|
-| `textDocument/completion`     | Complete              |
-| `textDocument/definition`     | Go to definition      |
-| `textDocument/typeDefinition` | Go to type definition |
-| `textDocument/hover`          | Show documentation    |
-| `textDocument/references`     | Show references       |
+| LSP method                    | Description                |
+|-------------------------------|----------------------------|
+| `textDocument/completion`     | Complete                   |
+| `textDocument/definition`     | Go to definition           |
+| `textDocument/typeDefinition` | Go to type definition      |
+| `textDocument/hover`          | Show documentation         |
+| `textDocument/references`     | Show references            |
+| `textDocument/rename`         | Renaming symbols and files |
 
 
 
@@ -32,19 +33,21 @@ About
 
 ### Why?
 
-General-purpose servers (pyls, py-lsp) try to do too much and break stuff too
-often for me. Locking Neovim when I press tab, crashes of all kind, LspRestart
-failing. Also I like my linting and formatting done by dedicated tools such as
-[nvim-lint][nvim-lint] and [formatter][formatter].
+General-purpose servers (e.g. pyls, py-lsp) try to do too much and break stuff
+too often for me. Locking Neovim when I press tab, crashes of all kind,
+LspRestart failing. Also I like my linting and formatting done by dedicated
+tools such as [nvim-lint][nvim-lint] and [formatter][formatter].
 
 [nvim-lint]: https://github.com/mfussenegger/nvim-lint
 [formatter]: https://github.com/mhartington/formatter.nvim
 
-Other Jedi-based servers seem to focus on coc-nvim and frequently fail on
-Neovim's native LSP client for me. I tried to fix jedi-language-server several
-times when it failed me but thought it could be fun to try pygls to redo it as
-small and simple as I can. And running a Node server to get Python completions?
-HA!
+Other Jedi-based servers (e.g. jedi-language-server) seem to focus on coc-nvim
+and frequently fail on Neovim's native LSP client for me. I tried to fix
+jedi-language-server several times when it failed me but I thought it could be
+fun to try pygls to redo it as small and simple as I can. And running a Node
+server to get Python completions? No way. That said, jedi-language-server is a
+good project and if you're fine with coc-nvim you should definitely check it
+out. Lots of the code here is ~~stolen~~ inspired from this project.
 
 ### Why the name?
 
--- italianswirls/server.py
+++ italianswirls/server.py
@@ -2,35 +2,22 @@ import logging
 from typing import Optional
 
 from jedi import Script
-from jedi.api.classes import Name
+from jedi.api.refactoring import RefactoringError
 from pygls.lsp.methods import (COMPLETION, DEFINITION, HOVER, REFERENCES,
-                               TYPE_DEFINITION)
-from pygls.lsp.types import (CompletionItem, CompletionItemKind,
-                             CompletionList, CompletionOptions,
+                               RENAME, TYPE_DEFINITION)
+from pygls.lsp.types import (CompletionItem, CompletionList, CompletionOptions,
                              CompletionParams, DefinitionParams, Hover,
-                             HoverParams, InsertTextFormat,
-                             Location, Position, Range, ReferenceParams,
-                             TextDocumentPositionParams, TypeDefinitionParams)
+                             HoverParams, InsertTextFormat, Location,
+                             ReferenceParams, RenameParams,
+                             TextDocumentPositionParams, TypeDefinitionParams,
+                             WorkspaceEdit)
 from pygls.server import LanguageServer
 from pygls.workspace import Document
 
-LOG = logging.getLogger()
-LOG.setLevel(logging.DEBUG)
-LOG.addHandler(logging.FileHandler("/tmp/a.log"))
+from italianswirls.glue import (gen_document_edits, get_jedi_position,
+                                get_lsp_completion_kind, get_lsp_locations)
 
 LS = LanguageServer('italianswirls', 'v0.0.1')
-
-JEDI_COMPLETION_TYPE_MAP = {
-    "module": CompletionItemKind.Module,
-    "class": CompletionItemKind.Class,
-    "instance": CompletionItemKind.Variable,
-    "function": CompletionItemKind.Function,
-    "param": CompletionItemKind.Variable,
-    "path": CompletionItemKind.File,
-    "keyword": CompletionItemKind.Keyword,
-    "property": CompletionItemKind.Property,
-    "statement": CompletionItemKind.Variable,
-}
 
 
 def get_jedi_script(document: Document) -> Script:
@@ -47,54 +34,6 @@ def get_jedi_script_from_params(
     document = server.workspace.get_document(document_uri)
     script = get_jedi_script(document)
     return script
-
-
-def get_jedi_position(position: Position) -> tuple[int, int]:
-    """Translate LSP Position to Jedi position (where line is 1-based)."""
-    return position.line + 1, position.character
-
-
-def get_lsp_position(line: int, column: int) -> Position:
-    """Translate Jedi position to LSP Position (where line is 0-based)."""
-    return Position(line=line - 1, character=column)
-
-
-def get_lsp_range(name: Name) -> Optional[Range]:
-    """Get an LSP range for this name, if it has a location."""
-    if name.line is None or name.column is None:
-        return None
-    start_position = get_lsp_position(name.line, name.column)
-    end_position = get_lsp_position(name.line, name.column + len(name.name))
-    return Range(start=start_position, end=end_position)
-
-
-def get_lsp_location(name: Name) -> Optional[Location]:
-    """Return an LSP location from this Jedi Name."""
-    if name.module_path is None:
-        return None
-    if (lsp_range := get_lsp_range(name)) is None:
-        return None
-    return Location(uri=name.module_path.as_uri(), range=lsp_range)
-
-
-def get_lsp_locations(names: list[Name]) -> list[Location]:
-    """Return a list of LSP locations from this list of Jedi Names.
-
-    Names that cannot be converted to a LSP location are discarded.
-    """
-    lsp_locations = []
-    for name in names:
-        if lsp_location := get_lsp_location(name):
-            lsp_locations.append(lsp_location)
-    return lsp_locations
-
-
-def get_lsp_completion_kind(jedi_compl_type: str) -> CompletionItemKind:
-    """Return an LSP completion item kind from this Jedi completion type."""
-    return JEDI_COMPLETION_TYPE_MAP.get(
-        jedi_compl_type,
-        CompletionItemKind.Text
-    )
 
 
 @LS.feature(COMPLETION, CompletionOptions(trigger_characters=["."]))
@@ -203,5 +142,20 @@ async def do_hover(
     return Hover(contents=hover_text)
 
 
-if __name__ == "__main__":
-    LS.start_io()
+@LS.feature(RENAME)
+async def do_rename(
+    server: LanguageServer,
+    params: RenameParams,
+) -> Optional[WorkspaceEdit]:
+    """Ask Jedi to rename a symbol and return the resulting state."""
+    script = get_jedi_script_from_params(params, server)
+    jedi_position = get_jedi_position(params.position)
+    try:
+        refactoring = script.rename(*jedi_position, new_name=params.new_name)
+    except RefactoringError as exc:
+        logging.error(f"Refactoring failed: {exc}")
+        return None
+
+    changes = list(gen_document_edits(refactoring, server.workspace))
+    logging.info(f"changes: {changes}")
+    return WorkspaceEdit(document_changes=changes) if changes else None
