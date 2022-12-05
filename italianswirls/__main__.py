dc7c7a877e45f293f430de8c22f005b87168c8aa
dece <shgck@pistache.land>
2022-12-05 11:04:06 +0100

    cli: move entrypoint and add propre debug flag

--- italianswirls/__main__.py
+++
@@ -1,8 +0,0 @@
-from italianswirls.server import LS
-
-if __name__ == "__main__":
-    import logging
-    root_logger = logging.getLogger()
-    root_logger.setLevel(logging.DEBUG)
-    root_logger.addHandler(logging.FileHandler("/tmp/a.log"))
-    LS.start_io()
